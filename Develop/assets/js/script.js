cbc8f22ff4d84fd864031f6605edc5628e757783
ariellongoria <ariello97@hotmail.com>
2021-04-22 20:26:00 -0500

    event persistence

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,43 @@
+$("#currentDay").html(moment().format("dddd, MMMM Do"));
+
+events = [];
+
+var saveEvents = function () {
+    localStorage.setItem("events", JSON.stringify(events));
+};
+
+var loadEvents = function () {
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+    if (storedEvents) {
+        for (i = 0; i < storedEvents.length; i++) {
+            $("#" + storedEvents[i].id + " textarea").val(storedEvents[i].text);
+        }
+    }
+    events = storedEvents;
+};
+
+var currentHour = moment().format("H");
+
+for (hour = 9; hour < 18; hour++) {
+    if (currentHour > hour) {
+        $("#" + hour + " textarea").addClass("past");
+    } else if (currentHour === hour) {
+        $("#" + hour + " textarea").addClass("present");
+    } else {
+        $("#" + hour + " textarea").addClass("future");
+    }
+}
+
+loadEvents();
+
+$(".saveBtn").click(function () {
+    var parentId = $(this).parent().attr("id");
+    var eventText = $("#" + parentId + " textarea").val();
+    newEvent = {
+        id: parentId,
+        text: eventText,
+    };
+    events.push(newEvent);
+
+    saveEvents();
+});
